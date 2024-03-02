4e2c7a341f0257d294306e5d23b3efad3966c185
Madina Yantay <madinayantay@gmail.com>
2024-03-02 19:16:05 +0500

    feat: add postgres db connection

---
+++ internal/repository/postgres/postgres.go
@@ -0,0 +1,43 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq" // init driver
+	"github.com/yantay0/url-shortener/internal/config"
+)
+
+type Repository struct {
+	db *sql.DB
+}
+
+func OpenDB(cfg *config.Config) (*Repository, error) {
+
+	const op = "repository.postgres.OpenDB"
+	db, err := sql.Open("postgres", cfg.DB.Dsn)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	db.SetMaxOpenConns(cfg.DB.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.DB.MaxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.DB.MaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Repository{db: db}, nil
+}
