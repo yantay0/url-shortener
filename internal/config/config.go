cd016cdb3ee09c78d216a4a30bb82fbc4e640f58
Madina Yantay <madinayantay@gmail.com>
2024-03-03 14:45:07 +0500

    refactor: separate address into ip and port

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,7 +22,8 @@ type DB struct {
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	IpAdress    string        `yaml:"ip_address" env-default:"localhost"`
+	Port        int           `yaml:"port" env-default:"8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
