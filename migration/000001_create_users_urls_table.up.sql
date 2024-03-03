98498677a127c32ba655adb043d60920b2e2903e
Madina Yantay <madinayantay@gmail.com>
2024-03-03 14:56:06 +0500

    feat: add migration (will be changed)

--- migration/000001_create_users_urls_table.up.sql
+++ migration/000001_create_users_urls_table.up.sql
@@ -1,17 +1,18 @@
 CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email TEXT NOT NULL,
-    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    role VARCHAR(255) NOT NULL DEFAULT 'USER'
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name text NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    activated bool NOT NULL,
+    version integer NOT NULL DEFAULT 1
 );
 
-CREATE TABLE IF NOT EXISTS urls (
-    id BIGSERIAL PRIMARY KEY,
-    short_url TEXT NOT NULL,
-    original_url TEXT NOT NULL,
-    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS url (
+    id bigserial PRIMARY KEY,
+    short_url text NOT NULL,
+    original_url text NOT NULL,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    -- user_id BIGINT NOT NULL,
+    -- FOREIGN KEY (user_id) REFERENCES users(id)
 )
