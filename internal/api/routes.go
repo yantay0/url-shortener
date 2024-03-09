9e695763632590133f89dedf52b02fe4b0aa8e09
Madina Yantay <madinayantay@gmail.com>
2024-03-09 10:26:24 +0500

    feat: add base url

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -6,16 +6,17 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+const BASE_URL = "/api/v1"
+
 func (app *App) Routes() *httprouter.Router {
 	router := httprouter.New()
-
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-	router.HandlerFunc(http.MethodGet, "/healthcheck", app.HealthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/urls", app.ListUrlsHandler)
-	router.HandlerFunc(http.MethodGet, "/urls/:id", app.ShowUrlHandler)
-	router.HandlerFunc(http.MethodPost, "/urls", app.CreateUrlHandler)
-	router.HandlerFunc(http.MethodPut, "/urls/:id", app.UpdateUrlHandler)
-	router.HandlerFunc(http.MethodDelete, "/urls/:id", app.DeleteUrlHandler)
+	router.HandlerFunc(http.MethodGet, BASE_URL+"/healthcheck", app.HealthcheckHandler)
+	router.HandlerFunc(http.MethodGet, BASE_URL+"/urls", app.ListUrlsHandler)
+	router.HandlerFunc(http.MethodGet, BASE_URL+"/urls/:id", app.ShowUrlHandler)
+	router.HandlerFunc(http.MethodPost, BASE_URL+"/urls", app.CreateUrlHandler)
+	router.HandlerFunc(http.MethodPut, BASE_URL+"/urls/:id", app.UpdateUrlHandler)
+	router.HandlerFunc(http.MethodDelete, BASE_URL+"/urls/:id", app.DeleteUrlHandler)
 	return router
 }
