475a81d745582cdcabe6d6814e1cfdf267c562bd
Madina Yantay <madinayantay@gmail.com>
2024-05-04 22:02:59 +0500

    feat: add gracefull shutdown

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,91 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"log/slog"
-	"net/http"
 	"os"
-	"time"
 
 	api "github.com/yantay0/url-shortener/internal/api"
 	"github.com/yantay0/url-shortener/internal/config"
+	"github.com/yantay0/url-shortener/internal/mailer"
 	"github.com/yantay0/url-shortener/internal/storage"
 
-	"github.com/yantay0/url-shortener/internal/lib/logger/handler/slogpretty"
-	"github.com/yantay0/url-shortener/internal/lib/logger/sl"
+	"github.com/yantay0/url-shortener/internal/lib/logger/jsonlog"
 	"github.com/yantay0/url-shortener/internal/storage/postgres"
 )
 
 // @title URL-shortener
 // @host localhost: 8085
 
-const (
-	envLocal = "local"
-	envDev   = "dev"
-	envProd  = "prod"
-)
+// const (
+// 	envLocal = "local"
+// 	envDev   = "dev"
+// 	envProd  = "prod"
+// 	version  = "1.0.0"
+// )
 
 func main() {
 	cfg := config.MustLoad()
-	log := setupLogger(cfg.Env)
 
-	log = log.With(slog.String("env", cfg.Env)) // current env is added to each log
-	log.Debug("debug messages are enabled")
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := postgres.OpenDB(cfg)
 	if err != nil {
-		log.Error("failed to open db connection", sl.Err(err))
-		os.Exit(1)
-	}
-
-	app := api.NewApp(*cfg, log, storage.NewModels(db))
-
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%s", cfg.HTTPServer.Port),
-		Handler:      app.Routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+		logger.PrintFatal(err, nil)
 	}
 
 	defer db.Close()
+	logger.PrintInfo("database conntection pool established", nil)
 
-	log.Info("starting server", cfg.Env, cfg.HTTPServer.Port)
-	err = srv.ListenAndServe()
-	log.Error("failed to start server", sl.Err(err))
-}
+	app := api.NewApp(*cfg, logger, storage.New(db), mailer.New(cfg.SMTP.Host, cfg.SMTP.Port, cfg.SMTP.Username, cfg.SMTP.Password, cfg.SMTP.Sender))
 
-func setupLogger(env string) *slog.Logger {
-	var log *slog.Logger
-	switch envLocal {
-	case envLocal:
-		log = setupPrettySlog()
-
-	case envDev:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
-	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
-	default:
-		log = slog.New(
-
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
+	err = app.Serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-	return log
-}
-
-func setupPrettySlog() *slog.Logger {
-	opts := slogpretty.PrettyHandlerOptions{
-		SlogOpts: &slog.HandlerOptions{
-			Level: slog.LevelDebug,
-		},
-	}
-
-	handler := opts.NewPrettyHandler(os.Stdout)
-
-	return slog.New(handler)
 }
---
+++ internal/api/server.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *App) Serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%s", app.Config.HTTPServer.Port),
+		Handler:      app.Routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	shutdownError := make(chan error)
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		app.Logger.PrintInfo("shutting down server", map[string]string{
+			"signal": s.String(),
+		})
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	app.Logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.Config.Env,
+	})
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.Logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
+}
+
+// when app receive a SIGINT or SIGTERM signal, we instruct ourserver
+// to stop accepting any new HTTP requests, and give any in-flight requests a ‘grace period’ of 5
+// secondsto complete before the application isterminated.
